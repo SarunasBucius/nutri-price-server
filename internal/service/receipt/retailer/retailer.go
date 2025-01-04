473101b5603f301dba32317387fa5f2d46b3f67a
sarunas <sarunas.bucius96@gmail.com>
2025-01-04 19:39:01 +0200

    add support for Lidl receipt parsing in retailer package

--- internal/service/receipt/retailer/retailer.go
+++ internal/service/receipt/retailer/retailer.go
@@ -1,19 +1,24 @@
 package retailer
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
 	"github.com/SarunasBucius/nutri-price-server/internal/model"
+	"github.com/SarunasBucius/nutri-price-server/internal/service/receipt/retailer/lidl"
 	"github.com/SarunasBucius/nutri-price-server/internal/service/receipt/retailer/norfa"
+	"github.com/SarunasBucius/nutri-price-server/internal/utils/uerror"
 )
 
 func NewReceiptParser(receipt string) (ReceiptParser, error) {
-	if strings.Contains(receipt, "UAB NORFOS MAŽMENA") {
+	switch {
+	case strings.Contains(receipt, "UAB NORFOS MAŽMENA"):
 		return norfa.NewParser(strings.Split(receipt, "\n")), nil
+	case strings.Contains(receipt, "Lidl Lietuva"):
+		return lidl.NewParser(strings.Split(receipt, "\n")), nil
+	default:
+		return nil, uerror.NewBadRequest("unknown retailer", nil)
 	}
-	return nil, fmt.Errorf("unknown retailer")
 }
 
 type ReceiptParser interface {
